cd0c7ac3e6d7cbda44a81fd7e96cbc0a33777b39
Louis Arbey <louis.arbey@efrei.net>
2022-10-24 12:14:18 +0200

    added a brief description

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ def main():
     st.markdown("")
     link = '[APP](https://whatsaid.streamlitapp.com/sentiment)'
     st.success("Go to the SENTIMENT "+ link +" 📚 by clicking on the sidebar on the left")
+    st.write("")
+    st.markdown("What'Said is an online app that allow you to analyse what is said of an entreprise on Twitter. You can choose the company you want to analyse and the number of tweets you want to analyse. The app will then analyse the tweets and give you a sentiment analysis of the tweets. You can also see the most common words used in the tweets and a wordcloud of the tweets.")
+    st.write("")
     st.image("images/logo_datacamp.png", use_column_width=True)
     st.write("")
     st.write('> *made by Thomas Billaudeau, Louis Arbey & Pierre-Louis Cretinon.*')
